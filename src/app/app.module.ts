57d45f852a47a4339e7d78586e90b43d62de3e9c
David Toledo <dtoledoz0207@gmail.com>
2019-02-21 23:03:43 -0600

    final-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
+import { Pagina2Component } from './pages/pagina2/pagina2.component';
+import { Pagina3Component } from './pages/pagina3/pagina3.component';
+
+import {AppRoutingModule} from './app.routes';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pagina1Component,
+    Pagina2Component,
+    Pagina3Component,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
